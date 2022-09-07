53c3d574c05a92955385bef9f93754ec848a84e9
Richard Neher <richard.neher@unibas.ch>
2022-09-07 17:09:15 +0200

    refactor sort script

--- ingest/bin/sort.py
+++ ingest/bin/sort.py
@@ -2,60 +2,75 @@ import numpy as np
 import pandas as pd
 from Bio import SeqIO
 from functools import reduce
+from collections import defaultdict
 
-dfs, a_list, b_list, a_sequences, b_sequences, listofdictionaries, a_metadata, b_metadata = ([] for i in range(8))
-to_a = ['a_new', 'a_middle', 'a_old']
-to_b = ['b_new', 'b_middle', 'b_old']
-seq = ["data/a/1_sequences.aligned.fasta", "data/a/2_sequences.aligned.fasta", "data/a/3_sequences.aligned.fasta", "data/b/1_sequences.aligned.fasta", "data/b/2_sequences.aligned.fasta", "data/b/3_sequences.aligned.fasta"]
-references = ["config/a_1_reference.fasta", "config/a_2_reference.fasta", "config/a_3_reference.fasta", "config/b_1_reference.fasta", "config/b_2_reference.fasta", "config/b_3_reference.fasta"]
-accessions = [pd.read_csv("data/a/1_metadata.tsv", sep='\t').accession, pd.read_csv("data/a/2_metadata.tsv", sep='\t').accession, pd.read_csv("data/a/3_metadata.tsv", sep='\t').accession, pd.read_csv("data/b/1_metadata.tsv", sep='\t').accession, pd.read_csv("data/b/2_metadata.tsv", sep='\t').accession, pd.read_csv("data/b/3_metadata.tsv", sep='\t').accession] 
-everything = (set().union(*accessions))
+def sequence_to_int_array(s, fill_value=110, fill_gaps=True):
+    seq = np.frombuffer(str(s).lower().encode('utf-8'), dtype=np.int8).copy()
+    if fill_gaps:
+        seq[(seq!=97) & (seq!=99) & (seq!=103) & (seq!=116)] = fill_value
+    else:
+        seq[(seq!=97) & (seq!=99) & (seq!=103) & (seq!=116) & (seq!=45)] = fill_value
+    return seq
 
-for file_, reference in zip(seq, references):
-    dictionary = dict()
-    accessions_, values = ([] for i in range(2))
-    file = SeqIO.parse(file_,"fasta")
-    ref_array = np.array(SeqIO.read(reference,"fasta").seq)
+def get_similarity(alignment, reference):
+    ref_array = sequence_to_int_array(SeqIO.read(reference,"fasta").seq, fill_gaps=False)
+    similarity = {}
+    for seq in SeqIO.parse(alignment,"fasta"):
+        seq_array = sequence_to_int_array(seq.seq, fill_gaps=False)
+        good_indices = seq_array!=45
+        similarity[seq.id] = np.mean(seq_array[good_indices]==ref_array[good_indices])
 
-    for record in file:
-        record_array = np.array(record.seq)
-        good_indices = (ref_array!='-')&(record_array!='-')
-        mean = np.mean(record_array[good_indices]==ref_array[good_indices])
-        accessions_.append(record.id)
-        values.append(mean)
+    return similarity
 
-    for accession in everything:
-        if accession not in accessions_:
-            accessions_.append(accession)
-            values.append(0.001)
+if __name__=="__main__":
+    dfs, a_list, b_list, a_sequences, b_sequences, listofdictionaries, a_metadata, b_metadata = ([] for i in range(8))
+    to_a = ['a_1', 'a_2', 'a_3']
+    to_b = ['b_1', 'b_2', 'b_3']
+    seq = ["data/a/1_sequences.aligned.fasta", "data/a/2_sequences.aligned.fasta", "data/a/3_sequences.aligned.fasta",
+           "data/b/1_sequences.aligned.fasta", "data/b/2_sequences.aligned.fasta", "data/b/3_sequences.aligned.fasta"]
+    references = ["config/a_1_reference.fasta", "config/a_2_reference.fasta", "config/a_3_reference.fasta",
+                  "config/b_1_reference.fasta", "config/b_2_reference.fasta", "config/b_3_reference.fasta"]
+    accessions = [pd.read_csv("data/a/1_metadata.tsv", sep='\t').accession,
+                  pd.read_csv("data/a/2_metadata.tsv", sep='\t').accession,
+                  pd.read_csv("data/a/3_metadata.tsv", sep='\t').accession,
+                  pd.read_csv("data/b/1_metadata.tsv", sep='\t').accession,
+                  pd.read_csv("data/b/2_metadata.tsv", sep='\t').accession,
+                  pd.read_csv("data/b/3_metadata.tsv", sep='\t').accession]
+    everything = set().union(*accessions)
 
-    dictionary['accession'] = accessions_
-    dictionary['value'] = values
-    listofdictionaries.append(dictionary)
+    all_similarities = defaultdict(list)
+    for filename, reference in zip(seq, references):
+        similarity = get_similarity(filename, reference)
 
-for i, column in zip(listofdictionaries, (to_a+to_b)):
-    df = pd.DataFrame(i).groupby('accession', group_keys=False).apply(lambda x: x.loc[x.value.idxmax()]).iloc[:,1:].reset_index().sort_values('accession').rename(columns={"value": column})
-    dfs.append(df)
-merged_df = reduce(lambda l, r: pd.merge(l, r, on='accession'), dfs)
+        for accession in everything:
+            all_similarities[accession].append(similarity.get(accession, 0.001))
 
-df_drop = merged_df.iloc[:, 1:]
-first_column = (merged_df.iloc[:, 0])
-df_values = (df_drop.idxmax(axis=1)).rename("values")
-result = pd.concat([first_column,df_values], axis=1)
+    all_similarities_df = pd.DataFrame(all_similarities, index = to_a + to_b).T
 
-for acc, a_or_b in zip(result['accession'], result['values']):
-    if a_or_b in to_a: a_list.append(acc)
-    elif a_or_b in to_b: b_list.append(acc)
+    max_similarity = all_similarities_df.max(axis=1)
 
-for record in SeqIO.parse("data/sequences.fasta", "fasta"):
-    if record.id in a_list: a_sequences.append(record)
-    if record.id in b_list: b_sequences.append(record)
+    all_similarities_df = all_similarities_df.loc[max_similarity>0.70,:]
 
-SeqIO.write(a_sequences, "data/a/sequences_notdedup.fasta","fasta")
-SeqIO.write(b_sequences, "data/b/sequences_notdedup.fasta", "fasta")
+    a_or_b = all_similarities_df.idxmax(axis=1).apply(lambda x: 'A' if x[0]=='a' else 'B')
 
-tsv_file = pd.read_csv("data/metadata.tsv", sep="\t")
-a_metadata = pd.DataFrame(data =tsv_file.loc[tsv_file['accession'].isin(a_list)], columns=tsv_file.columns)
-b_metadata = pd.DataFrame(data=tsv_file.loc[tsv_file['accession'].isin(b_list)], columns=tsv_file.columns)
-a_metadata.to_csv('data/a/metadata_notdedup.tsv', sep="\t")
-b_metadata.to_csv('data/b/metadata_notdedup.tsv', sep="\t")
+    for record in SeqIO.parse("data/sequences.fasta", "fasta"):
+        if record.id in a_or_b:
+            if a_or_b[record.id]=='A':
+                a_sequences.append(record)
+            elif a_or_b[record.id]=='B':
+                b_sequences.append(record)
+
+    SeqIO.write(a_sequences, "data/a/sequences_notdedup.fasta","fasta")
+    SeqIO.write(b_sequences, "data/b/sequences_notdedup.fasta", "fasta")
+
+    metadata = pd.read_csv("data/metadata.tsv", sep="\t", index_col='accession')
+    original_columns = metadata.columns
+    metadata.drop_duplicates(keep='first', inplace=True)
+    metadata['type'] = a_or_b
+
+    a_metadata = pd.DataFrame(data=metadata.loc[metadata['type']=='A'],
+                              columns=original_columns)
+    b_metadata = pd.DataFrame(data=metadata.loc[metadata['type']=='B'],
+                              columns=original_columns)
+    a_metadata.to_csv('data/a/metadata_notdedup.tsv', sep="\t")
+    b_metadata.to_csv('data/b/metadata_notdedup.tsv', sep="\t")
